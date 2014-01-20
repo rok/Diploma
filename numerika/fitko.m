c6c485fa1826b4126e892e60bd2797e1cad8e9f1
Rok Mihevc <rok@mihevc.org>
2014-01-20 13:09:53 +0100

    added some mathematica stuff

--- numerika/fitko.m
+++ numerika/fitko.m
@@ -1,21 +1,22 @@
-load /home/user/diploma/numerika/menisija-profil-profilov.mat
+load menisija-profil-profilov.mat
 
-if 1 # Fitting
+if 1 % Fitting
     ps = zeros(4,60-5);
     for j=5:60
-        ## independents and observations
+        % independents and observations
         indep = 0:(size(nonzeros(profi(:,j))',2)-1);
         obs = profi(1:j,j)';
         obs = obs - min(obs);
 	
         f = @ (p, x) p(1) *(1 - exp (-(p(2) * (x-p(3))).^2))+p(4);
-        ## initial values:
+        % initial values:
         init = [max(obs); 1; 0;0];
     
-        ## start optimization
+        % start optimization
         [p, model_values, cvg, outp] = nonlin_curvefit (f, init, indep, obs)
         ps(:,j-4) = p;
-        if 0 % j==22 # plotting
+        if 1
+            %j=22 % plotting
             plot(obs,'linewidth',1,'b','marker','+');
             hold on;
             plot(model_values,'linewidth',2,'r');
@@ -24,11 +25,13 @@ if 1 # Fitting
             xlabel('Polmer [m]')
             ylabel('Visina [m]')
             legend('Povprecni profil','Nalozena funkcija','location','southeast')
-            print ../Latex/slike/menisija-profil-21-fit.eps -depsc "-S900,400"
+            %print ../Latex/slike/menisija-profil-21-fit.eps -depsc "-S900,400"
             pause
         end
     end
 end
+
+%%
 
 if 0
     plot(3:size(ps,2),sqrt(abs(ps(2,3:end))))
@@ -38,16 +41,16 @@ if 0
     print ../Latex/slike/menisija-sigme.eps -depsc "-S900,500"
 end
 
-if 0 # Plot sigma od r + fit
-    ## independents and observations
+if 0 % Plot sigma od r + fit
+    % independents and observations
     indep = 0:(size(ps,2)-3);
     obs = sqrt(abs(ps(2,3:end)));
 	
     f = @ (p, x) p(1) *exp (-(p(2) * (x-p(3)))) + p(4);
-    ## initial values:
+    % initial values:
     init = [max(obs);0.1;0;0.1];
     
-    ## start optimization
+    % start optimization
     [p, model_values, cvg, outp] = nonlin_curvefit (f, init, indep, obs)
 
     plot(3:size(ps,2),model_values,'linewidth',2,'r');
